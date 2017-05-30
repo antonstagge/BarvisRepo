c4a11166c1e47d1d01185c42b526bbb037e235a3
Anton Stagge <anton.stagge@winfallvr.com>
2017-05-30 17:59:31 +0200

    Added first GET test.

--- client_barvis/main.py
+++ client_barvis/main.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,6 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "This is the homepage"
+
+@app.route('/testReq')
+def testReq():
+    bla = requests.get("https://httpbin.org/get")
+    print(bla.url)
+    return "succesfull get"
 
 if __name__ == "__main__":
     app.run(debug=True)
